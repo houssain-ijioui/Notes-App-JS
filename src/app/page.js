ede2a2b5df7f7166d0d19061b87041b72b509f3d
Lahoucine Ijioui <ije1work@gmail.com>
2024-02-29 19:11:16 +0100

    show notes in main page

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { getNotes } from "@/store/features/noteAction";
 import Navbar from "@/components/Navbar";
 import NoteCard from "@/components/NoteCard";
-
+import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
   const notes = useSelector((state) => state.notes.notes)
   const dispatch = useDispatch()
-
-
 
   useEffect(() => {
     dispatch(getNotes())
@@ -20,21 +18,12 @@ export default function Home() {
       <Navbar />
       <div className="flex flex-row">
         <Sidebar />
-        <div className="grid grid-cols-3 gap-4 mt-4 ml-3 px-4">
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
-          <NoteCard />
+        <div className="grid grid-cols-3 gap-4 mt-4 ml-3 py-5 px-4">
+          {notes.map((note, index) => {
+            return (
+              <NoteCard key={index} description={note.description} createdAt={note.createdAt} />
+            )
+          })}
         </div>
       </div>
     </div>
